da6a147716e0e1fb15ba4798f37aeb10a84f346a
Rudhra0219 <rudhrapriya02@gmail.com>
2026-04-16 08:14:49 +0530

    Create qn.java

---
+++ qn.java
@@ -0,0 +1,23 @@
+Hotel Room Booking Management System
+Problem Statement:
+Ensure room bookings are managed without double-booking and maintain accurate
+availability status.
+Tasks:
+1. Develop room booking and cancellation logic
+2. Write JUnit tests for booking success/failure scenarios
+3. Implement CI/CD pipeline with automated testing
+o Set up version control using GitHub
+o Configure CI pipeline (Jenkins / GitHub Actions)
+o Automate build using Apache Maven
+o Integrate unit testing
+o Configure CD pipeline for deployment
+4. Deploy services on Docker and Kubernetes
+o Containerize using Docker
+o Push image to Docker Hub
+o Create Kubernetes deployment and service YAML files
+Components Marks
+Java Program Development 15
+Junit Testing 15
+CI/CD Pipeline Creation and Execution 10
+Deploy services on Docker and Kubernetes 10
+Total: 5
